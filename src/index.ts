e8c955b19b7f89f9479e930c8a999a50468e52da
Julien Capellari <julien.capellari@gmail.com>
2023-10-14 14:25:47 +0200

    feat: add include option

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,23 @@
 import { transform, Options } from '@swc/core';
 import { type Plugin } from 'vite';
 
+// Types
+export interface SwcOptions extends Options {
+  include?: RegExp;
+}
+
 // Plugin
-export function swc(opts: Options = {}): Plugin {
+export function swc(opts: SwcOptions = {}): Plugin {
+  const include = opts.include ?? /\.[jt]sx?$/;
+
   return {
     name: 'jujulego:swc',
     enforce: 'pre',
     async transform(code, id) {
+      if (!include.exec(id)) {
+        return;
+      }
+
       return await transform(code, {
         swcrc: true,
         ...opts,
